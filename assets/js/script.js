b7c19cd818fbd5750bba5a1a26e8bfe7ff6f258e
Rodney-Makomo <rodneymakomo@gmail.com>
2021-11-30 19:07:53 +0000

    add array of questions to script

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+//Quiz Questions
+const dataQuiz = [
+    {
+        question: 'On what date was the pilot episode "pilot" aired?',
+        a: '4 September 2012',
+        b: '24 September 2009',
+        c: '17 September 2011',
+        d: '24 Septmeber 2007'
+    }, {
+        question: 'In which season did Raj get over his anxiety of speaking to girls?',
+        a: 'Season 3',
+        a: 'Season 6',
+        a: 'Season 7',
+        a: 'Season 5'
+    }, {
+        question: 'In which season did Penny and Leonard first engage in "coitus"?',
+        a: 'Season One, The Middle Earth Paradigm',
+        b: 'Season Five, The Recombination Hypothesis',
+        c: 'Season Six, The 43 Peculiarity',
+        d: 'Season Four, The 21-Second Excitation'
+    }
+]
