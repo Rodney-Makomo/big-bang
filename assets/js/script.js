112ff0a58a663399737ac3da7be093a5799010e8
Rodney-Makomo <rodneymakomo@gmail.com>
2021-12-01 19:22:59 +0000

    add questions to array

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,18 +5,42 @@ const dataQuiz = [
         a: '4 September 2012',
         b: '24 September 2009',
         c: '17 September 2011',
-        d: '24 Septmeber 2007'
+        d: '24 Septmeber 2007',
+        correct: 'd'
     }, {
         question: 'In which season did Raj get over his anxiety of speaking to girls?',
         a: 'Season 3',
-        a: 'Season 6',
-        a: 'Season 7',
-        a: 'Season 5'
+        b: 'Season 6',
+        c: 'Season 7',
+        d: 'Season 5',
+        correct: 'c'
     }, {
         question: 'In which season did Penny and Leonard first engage in "coitus"?',
         a: 'Season One, The Middle Earth Paradigm',
         b: 'Season Five, The Recombination Hypothesis',
         c: 'Season Six, The 43 Peculiarity',
-        d: 'Season Four, The 21-Second Excitation'
+        d: 'Season Four, The 21-Second Excitation',
+        correct: 'b'
+    }, {
+        question: "Which apartment number was Leonard and Sheldon's apartment?",
+        a: '4B',
+        b: '3B',
+        c: '4A',
+        d: '5B',
+        correct: 'a'
+    }, {
+        question: 'How many languages did Howard Wolowitz speak?',
+        a: 'Three',
+        b: 'One',
+        c: 'Six',
+        d: 'Two',
+        correct: 'c'
+    }, {
+        question: 'From which state was Leonard Hofstadter originally from?',
+        a:'Texas',
+        b:'Nebraska',
+        c:'California',
+        d:'New Jersey',
+        correct: 'd'
     }
 ]
